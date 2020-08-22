90ffb40d7ec8db6ad073f08b34d0187f3df31f6e
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-22 19:52:00 -0300

    commit

--- dados.py
+++ dados.py
@@ -8,8 +8,15 @@ def tratamento_datas():
             empresa = []
             empresa.append(resultados[0])
             empresa.append(resultados[12])
+            lista_de_resultados.append(empresa)
+            del(empresa[:])
+            empresa.append(resultados[0])
             empresa.append(resultados[14])
+            lista_de_resultados.append(empresa)
+            del(empresa[:])
+            empresa.append(resultados[0])
             empresa.append(resultados[16])
             lista_de_resultados.append(empresa)
-    del(lista_de_resultados[0:12])
+
+    del(lista_de_resultados[0:10])
     return lista_de_resultados
--- edit_event.py
+++ edit_event.py
@@ -1,16 +1,31 @@
 from dados import *
 from datetime import datetime, timedelta
+import time
+
+lista2 = []
+
+
+def tratamento_de_data(data):
+    data = data[:10].split('-')
+    data[1] = data[1].replace('0','')
+    data[2]= data[2].replace('0','')
+    return f'{data[2]}/{data[1]}/{data[0]}'
 
 
 def editar_evento(service):
     page_token = None
+    lista1 = tratamento_datas()
     while True:
-      events = service.events().list(calendarId='primary', pageToken=page_token).execute()
-      for event in events['items']:
-        if event['summary'] == 'Resultados da VIVER':
-        #    eventId = event['id']
-        #    service.events().delete(calendarId='primary', eventId=eventId).execute()
-            print(event['summary'])
-      page_token = events.get('nextPageToken')
-      if not page_token:
-        break
+        events = service.events().list(calendarId='primary', pageToken=page_token).execute()
+        for event in events['items']:
+            #time.sleep(1)
+            lista = []
+            lista.append(event['summary'])
+            lista.append(tratamento_de_data(event['start']['dateTime']))
+            lista2.append(lista)
+        page_token = events.get('nextPageToken')
+        if not page_token:
+            break
+    x = [li for li in lista1 if li not in lista2]
+    print(len(lista1), len(lista2))
+    #print(x)
