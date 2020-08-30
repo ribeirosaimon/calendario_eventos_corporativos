401d60d89b3a86f21fe0d1950237dfe1edd3a434
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-30 10:01:34 -0300

    Comit

--- quickstart.py
+++ quickstart.py
@@ -34,13 +34,11 @@ def main():
     baixar_arquivo(site_b3)
     editar_evento(service)
 
-#schedule.every().day.at("23:00").do(main)
+schedule.every().day.at("23:00").do(main)
 #schedule.every(1).minutes.do(main)
 
 if __name__ == '__main__':
-    main()
-
-    #while True:
-        #schedule.run_pending()
-        #main()
-        #time.sleep(30)
+    #main()
+    while True:
+        schedule.run_pending()
+        time.sleep(30)
