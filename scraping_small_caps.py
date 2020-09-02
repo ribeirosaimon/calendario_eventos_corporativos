5c3e3d5e6f461a96c856d10232f7d50c80d01350
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-09-02 16:01:48 -0300

    Update scraping_small_caps.py

--- scraping_small_caps.py
+++ scraping_small_caps.py
@@ -1,9 +1,15 @@
+
 from requests import get
 from bs4 import BeautifulSoup
-import time
 
-url_small_caps  = 'http://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-de-segmentos-e-setoriais/indice-small-cap-smll-composicao-da-carteira.htm'
-time.sleep(5)
+url_small_caps  = 'http://bvmf.bmfbovespa.com.br/indices/ResumoCarteiraTeorica.aspx?Indice=SMLL&idioma=pt-br'
+carteira_small = []
+
 browser = BeautifulSoup(get(url_small_caps).content, "html.parser")
-base = browser.find('tbody')
-print(base)
+base = browser.find('tbody').findAll('tr')
+for tr_soup in base:
+    td_soup = tr_soup.findAll('td')
+    carteira_small.append(td_soup[1].text.replace(' ',''))
+
+
+print(carteira_small)
